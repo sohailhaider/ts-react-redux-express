45a8026fb84059fd5e53f1bc5effa5695fabff62
Sohail Haider <sohailh343@gmail.com>
2021-03-16 18:02:47 +0500

    redux-thunks fetching data

--- my-app/src/features/itunes/itunesSlice.ts
+++ my-app/src/features/itunes/itunesSlice.ts
@@ -20,16 +20,6 @@ export const itunesSlice = createSlice({
     name: 'itunes',
     initialState,
     reducers: {
-    // increment: state => {
-    //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
-    //   // doesn't actually mutate the state because it uses the Immer library,
-    //   // which detects changes to a "draft state" and produces a brand new
-    //   // immutable state based off those changes
-    //   state.value += 1;
-    // },
-    // decrement: state => {
-    //   state.value -= 1;
-    // },
         setIsLoading: (state, action: PayloadAction<boolean>) => {
             state.isLoading = action.payload
         },
@@ -47,10 +37,6 @@ export const itunesSlice = createSlice({
 
 export const { setResults, setMessage, setIsLoading, setIsSearchedTrue } = itunesSlice.actions;
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
 export const searchByTerm = (searchTerm: string): AppThunk => dispatch => {
     
     dispatch(setIsLoading(true))
