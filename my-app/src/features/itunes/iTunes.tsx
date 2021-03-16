8a330b6339029d30c1bd8dc25a48a8e9d2f1b6a9
Sohail Haider <sohailh343@gmail.com>
2021-03-16 16:27:13 +0500

    Form ui complete

--- my-app/src/features/itunes/iTunes.tsx
+++ my-app/src/features/itunes/iTunes.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField'
-import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 
 export function ITunes() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const handleOnSubmit = (e:any):void => {
+        e.preventDefault();
+        console.log(searchTerm)
+    }
+    return (
+    <div>
+        <form onSubmit={handleOnSubmit}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Box mt={10}>
+                        <Typography variant="h2" align="center" gutterBottom>
+                            iTunes Search Engine
+                        </Typography>
+                    </Box>
+                </Grid>
 
-    return (<div>
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <Typography variant="h2" align="center" gutterBottom>
-                    iTunes Search Engine
-                </Typography>
-            </Grid>
-            <Grid item xs={2}>
+                <Grid item xs={2}>
+                </Grid>
+                <Grid item xs={8}>
+                    <TextField value={searchTerm} onChange={e=>setSearchTerm(e.target.value)} fullWidth={true} inputProps={{min: 0, style: { textAlign: 'center' }}} id="outlined-basic" label="Input Term to Search" variant="outlined" />
+                </Grid>
+                <Grid item xs={2}></Grid>
 
+                <Grid item xs={2}></Grid>
+                <Grid item xs={8}>
+                    <Box display="flex" flexDirection="row-reverse">
+                        <Button type="submit" variant="contained" color="primary" size="large" disableElevation>
+                            Search
+                        </Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={2}></Grid>
             </Grid>
-            <Grid item xs={8}>
-                <TextField fullWidth={true} inputProps={{min: 0, style: { textAlign: 'center' }}} id="outlined-basic" label="Input Term to Search" variant="outlined" />
-            </Grid>
-        </Grid>
+        </form>
     </div>)
 }
